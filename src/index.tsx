35aaaa4039f237a98dbd8a49d5f2a58b5bad4c89
Courtez <courtezj23@gmail.com>
2026-03-02 00:02:28 -0500

    using hash router instead of client route

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import App from './App'
 import './index.css'
 
@@ -8,8 +8,8 @@ const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
